e7e84e55211b3e99d58aacd4db01f05647dbd907
ChrisMuir <chrismuirRVA@gmail.com>
2017-11-07 15:53:33 -0600

    expand test coverage for merging ngram clusters

--- tests/testthat/test-n_gram_merge.R
+++ tests/testthat/test-n_gram_merge.R
@@ -5,7 +5,9 @@ context("n_gram_merge")
 vect <- c("Acmme Pizza, Inc.",
           "ACME PIZA COMPANY",
           "Acme Pizzazza LLC",
-          "acme pizza limited")
+          "acme pizza limited",
+          "Tom's Sports Equipment, Inc.",
+          "toms sports equipment")
 vect_ng <- n_gram_merge(vect,
                         numgram = 2,
                         edit_threshold = 1,
@@ -18,9 +20,9 @@ test_that("similar vals, output is a char vector", {
 
 test_that("similar vals, output lengths are correct", {
   expect_equal(length(vect_ng), length(vect))
-  expect_equal(length(unique(vect_ng)), 1)
-  expect_equal(length(unique(n_gram_merge(vect, numgram = 3))), length(vect))
-  expect_equal(length(unique(n_gram_merge(vect, edit_threshold = 5))), 1)
+  expect_equal(length(unique(vect_ng)), 2)
+  expect_equal(length(unique(n_gram_merge(vect, numgram = 3))), 5)
+  expect_equal(length(unique(n_gram_merge(vect, edit_threshold = 5))), 2)
   expect_equal(length(unique(n_gram_merge(vect, bus_suffix = FALSE))),
                length(vect))
 
@@ -29,14 +31,14 @@ test_that("similar vals, output lengths are correct", {
                                                 i = 0.2,
                                                 s = 1,
                                                 t = 1))
-  expect_equal(length(unique(vect_ng)), 2)
+  expect_equal(length(unique(vect_ng)), 3)
 
   vect_ng <- n_gram_merge(vect,
                           edit_dist_weights = c(d = 1,
                                                 i = 1,
                                                 s = 0.1,
                                                 t = 0.1))
-  expect_equal(length(unique(vect_ng)), 3)
+  expect_equal(length(unique(vect_ng)), 4)
 })
 
 
