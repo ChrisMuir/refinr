8bd3fa0d08f408e7ee996aa78b9c58c052264193
ChrisMuir <chrismuirRVA@gmail.com>
2017-12-11 01:44:35 -0600

    remove more unnecessary tests

--- tests/testthat/test-key_collision_merge.R
+++ tests/testthat/test-key_collision_merge.R
@@ -1,40 +1,26 @@
 context("key_collision_merge")
 
-## Tests using valid and similar input values.
-vect <- c("Acme Pizza, Inc.",
-          "Acme Pizza, Inc.",
-          "ACME PIZZA COMPANY",
+vect <- c("Acme Pizza, Inc.", "Acme Pizza, Inc.", "ACME PIZZA COMPANY",
           "acme pizza LLC")
-dict <- c("Nicks Pizza", "acme PIZZA inc")
-vect_kc <- key_collision_merge(vect, bus_suffix = TRUE)
+vect_kc <- key_collision_merge(vect)
 
-test_that("similar vals, output is a char vector", {
-  expect_is(vect_kc, "character")
-})
+test_that("similar vals, output is a char vector",
+          expect_is(vect_kc, "character"))
 
 test_that("similar vals, output lengths are correct", {
   expect_equal(length(vect_kc), length(vect))
   expect_equal(length(unique(vect_kc)), 1)
   expect_equal(
-    length(unique(key_collision_merge(vect, bus_suffix = FALSE))), 3
-  )
+    length(unique(key_collision_merge(vect, bus_suffix = FALSE))), 3)
 })
 
+dict <- c("Nicks Pizza", "acme PIZZA inc")
 test_that("param 'dict' having expected effect", {
-  expect_equal(unique(key_collision_merge(vect, dict = dict, bus_suffix = TRUE)), dict[2])
+  expect_equal(unique(key_collision_merge(vect, dict = dict)), dict[2])
 })
 
 vect <- c("Bakersfield Highschool", "BAKERSFIELD high", "high school, bakersfield")
-ignore_strings <- c("high", "school", "highschool")
+vect_ng <- key_collision_merge(vect, ignore_strings = c("high", "school", "highschool"))
 test_that("param 'ignore_strings' having expected effect", {
-  expect_equal(
-    unique(key_collision_merge(vect, ignore_strings = ignore_strings, bus_suffix = TRUE)),
-    vect[2]
-  )
-})
-
-## Tests using invalid input values.
-test_that("invalid input throws an error", {
-  expect_error(key_collision_merge(c(1, 2, 3, 4)))
-  expect_error(key_collision_merge(c("cats", "dogs"), bus_suffix = "true"))
+  expect_equal(unique(vect_ng), vect[2])
 })
--- tests/testthat/test-n_gram_merge.R
+++ tests/testthat/test-n_gram_merge.R
@@ -1,36 +1,21 @@
 context("n_gram_merge")
-#library(magrittr)
 
-## Tests using valid and similar input values.
-vect <- c("Acmme Pizza, Inc.",
-          "ACME PIZA COMPANY",
-          "Acme Pizzazza LLC",
-          "acme pizza limited",
-          "Tom's Sports Equipment, Inc.",
+vect <- c("Acmme Pizza, Inc.", "ACME PIZA COMPANY", "Acme Pizzazza LLC",
+          "acme pizza limited", "Tom's Sports Equipment, Inc.",
           "toms sports equipment")
-vect_ng <- n_gram_merge(vect,
-                        numgram = 2,
-                        edit_threshold = 1,
-                        edit_dist_weights = c(d = 0.2, i = 0.2, s = 1, t = 0.5),
-                        bus_suffix = TRUE)
+vect_ng <- n_gram_merge(vect)
 
-test_that("similar vals, output is a char vector", {
-  expect_is(vect_ng, "character")
-})
+test_that("output is a char vector", expect_is(vect_ng, "character"))
 
-test_that("similar vals, output lengths are correct", {
+test_that("output lengths are correct", {
   expect_equal(length(vect_ng), length(vect))
   expect_equal(length(unique(vect_ng)), 2)
   expect_equal(length(unique(n_gram_merge(vect, numgram = 3))), 5)
   expect_equal(length(unique(n_gram_merge(vect, edit_threshold = 5))), 2)
   expect_equal(length(unique(n_gram_merge(vect, bus_suffix = FALSE))),
                length(vect))
-
-  vect_ng <- n_gram_merge(vect,
-                          edit_dist_weights = c(d = 1,
-                                                i = 1,
-                                                s = 0.1,
-                                                t = 0.1))
+  vect_ng <- n_gram_merge(
+    vect, edit_dist_weights = c(d = 1, i = 1, s = 0.1, t = 0.1))
   expect_equal(length(unique(vect_ng)), 4)
 })
 
@@ -39,20 +24,7 @@ test_that("param 'edit_threshold' set to NA having expected effect", {
 })
 
 vect <- c("Bakersfield Highschool", "BAKERSFIELD high", "high school, bakersfield")
-ignore_strings <- c("high", "school", "highschool")
+vect_ng <- n_gram_merge(vect, ignore_strings = c("high", "school", "highschool"))
 test_that("param 'ignore_strings' having expected effect", {
-  expect_equal(
-    unique(n_gram_merge(vect, ignore_strings = ignore_strings, bus_suffix = TRUE)),
-    vect[2]
-  )
-})
-
-
-## Tests using invalid input values.
-test_that("invalid input throws an error", {
-  expect_error(n_gram_merge(c(1, 2, 3, 4)))
-  expect_error(n_gram_merge(c("cats", "dogs"), bus_suffix = "invalid"))
-  expect_error(n_gram_merge(c("cats", "dogs"), numgram = "invalid"))
-  expect_error(n_gram_merge(c("cats", "dogs"), edit_threshold = "invalid"))
-  expect_error(n_gram_merge(c("cats", "dogs"), edit_dist_weights = "invalid"))
+  expect_equal(unique(vect_ng), vect[2])
 })
