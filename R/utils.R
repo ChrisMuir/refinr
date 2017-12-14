f32d23b33375541b6dac3f52ac0de99b4199de14
ChrisMuir <chrismuirRVA@gmail.com>
2017-12-14 01:30:05 -0600

    use non-roxygen comments for internal funcs

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,5 @@
-#' Function that attempts to merge common business name suffixes within a
-#' character string.
-#' @param vect Character vector.
-#' @noRd
+# Function that attempts to merge common business name suffixes within a
+# character string.
 business_suffix <- function(vect) {
   vect %>%
     {gsub(" incorporated| incorporate", " inc", .)} %>%
@@ -15,13 +13,11 @@ business_suffix <- function(vect) {
     {gsub(" and ", " & ", ., fixed = TRUE)}
 }
 
-#' For each element of an input character vector, insert a single space
-#' between each char. This function is meant to mimic function ngram::splitter,
-#' but is faster due to fewer input checks.
-#' @param vect character vector.
-#' @param numgram_thres numeric value, after the splitting, any string that
-#'  has length less than this number will be replaced with NA_character_.
-#' @noRd
+# For each element of an input character vector, insert a single space
+# between each char. This function is meant to mimic function ngram::splitter,
+# but is faster due to fewer input checks.
+# Arg numgram_thres is a numeric value. After the splitting, any string that
+# has length less than this number will be replaced with NA_character_.
 char_splitter <- function(vect, numgram_thres) {
   vapply(vect, function(x) {
     x <- paste0(strsplit(x, split = "", fixed = TRUE)[[1]], collapse = " ")
@@ -33,11 +29,9 @@ char_splitter <- function(vect, numgram_thres) {
   }, character(1), USE.NAMES = FALSE)
 }
 
-#' Flatten a nested list such that each character vector occupies its own
-#' element in the return list. Can handle lists that have inconsistent nesting
-#' levels.
-#' @param list_obj list object.
-#' @noRd
+# Flatten a nested list such that each character vector occupies its own
+# element in the return list. Can handle lists that have inconsistent nesting
+# levels.
 flatten_list <- function(list_obj) {
   more_lists <- vapply(list_obj, is.list, logical(1), USE.NAMES = FALSE)
   out <- c(list_obj[!more_lists], unlist(list_obj[more_lists], FALSE, FALSE))
