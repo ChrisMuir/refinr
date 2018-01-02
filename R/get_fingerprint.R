974fe82dd64685fe0e7f8c1a2ff3dd19f1d3619e
ChrisMuir <chrismuirRVA@gmail.com>
2018-01-02 17:33:36 -0600

    consolidate both 'get_fingerprint_' funcs to one R file

--- R/get_fingerprint.R
+++ R/get_fingerprint.R
@@ -1,3 +1,38 @@
+#' Given a character vector as input, get the key collision fingerprint for
+#' each element.
+#' @noRd
+get_fingerprint_KC <- function(vect, bus_suffix = TRUE,
+                               ignore_strings = NULL) {
+  if (bus_suffix) {
+    vect <- business_suffix(tolower(vect))
+    if (!is.null(ignore_strings)) {
+      ignore_strings <- c(ignore_strings,
+                          c("inc", "corp", "co", "llc", "ltd", "div", "ent",
+                            "lp"))
+    } else {
+      ignore_strings <- c("inc", "corp", "co", "llc", "ltd", "div", "ent",
+                          "lp")
+    }
+  } else {
+    vect <- tolower(vect)
+  }
+  # Perform initial transformations.
+  vect <- vect %>%
+    gsub("[[:punct:]]", "", ., perl = TRUE) %>%
+    cpp_trimws_left %>%
+    strsplit(., "\\s+", perl = TRUE)
+  # If "ignore_strings" is not NULL, for each element of list "vect", remove
+  # any string that has a match within vector "ignore_strings".
+  if (!is.null(ignore_strings)) vect <- remove_strings(vect, ignore_strings)
+  # Final transformations, then return object "out".
+  vect <- vect %>%
+    cpp_list_unique(sort_vals = TRUE) %>%
+    vapply(., paste, character(1), collapse = " ") %>%
+    iconv(., to = "ASCII//TRANSLIT")
+  vect[vect == ""] <- NA_character_
+  return(vect)
+}
+
 #' Given a character vector as input, get the ngram fingerprint value for each
 #' element of the input.
 #'
