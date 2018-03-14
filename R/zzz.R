701bfc376d1e70bdea95cd4f04a37911b6b2ea61
ChrisMuir <chrismuirRVA@gmail.com>
2018-03-13 20:19:27 -0500

    comment tweak

--- R/zzz.R
+++ R/zzz.R
@@ -3,13 +3,12 @@
   library.dynam.unload("refinr", libpath)
 }
 
-# Set up pkg environment to house a list of acceptable input args for exported
-# function "n_gram_merge". Doing this because the ellipsis args get passed to
-# func stringdist::stringdistmatrix half-way through completion of
-# n_gram_merge, and it's preferable for n_gram_merge to fail immediately if
-# any args are passed that do not fit for either n_gram_merge nor
-# stringdistmatrix. This also has the added benefit of not failing if stringdist
-# updates their pkg and changes their func args.
+# Set up pkg environment to house a list of acceptable input args for
+# stringdist::stringdistmatrix. The ellipsis args for func "n_gram_merge()" get
+# passed to stringdistmatrix half-way through completion, and it's preferable
+# for n_gram_merge to fail immediately if any passed args are invalid. This
+# also has the added benefit of not failing if stringdist updates their pkg
+# and changes their func args.
 refinr_env <- new.env()
 sd_args <- names(formals(stringdist::stringdistmatrix))
 sd_args <- sd_args[!sd_args %in% c("a", "b", "weight")]
